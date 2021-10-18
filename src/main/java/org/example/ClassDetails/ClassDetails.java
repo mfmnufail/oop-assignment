041df34b8713df6a0e4c63aee2fa7393cbf108ba
Harshana96 <harshanajayathissa227@gmail.com>
2021-10-18 11:30:00 +0530

    Add class details

--- src/main/java/org/example/ClassDetails/ClassDetails.java
+++ src/main/java/org/example/ClassDetails/ClassDetails.java
@@ -1,13 +1,53 @@
 package org.example.ClassDetails;
 
+
 import org.example.Student.Student;
-import org.example.Student.StudentStore;
 
 import java.util.*;
 
-public class ClassDetails  {
+public class ClassDetails {
 
-    // Class detail goes here
-    // year, class name, teacher name,list of students 
+    private int year;
+    private final String className;
+    private String teacherName;
+    private final List<Student> students;
+    
 
-}
+
+
+    public ClassDetails(String className) {
+        this.className = className;
+        this.students = new ArrayList<>();
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setTeacherName(String name){
+        this.teacherName = name;
+    }
+
+
+
+    public int getYear() {
+        return year;
+    }
+
+     public String getClassName() {
+        return className;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+
+
+
+   }
+ 
