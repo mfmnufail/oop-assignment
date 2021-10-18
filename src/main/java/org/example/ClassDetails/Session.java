6db7f4c1ba902afe1910198919cf9dcedbfebe86
Mohamed Nufail <eg173254@engug.ruh.ac.lk>
2021-10-18 17:33:40 +0530

    Added session

--- src/main/java/org/example/ClassDetails/Session.java
+++ src/main/java/org/example/ClassDetails/Session.java
@@ -8,6 +8,74 @@ public class Session {
 
     // Session class contain feilds of session name, date, start time, end time
     // student attendance details 
+
+    private String sessionName;
+    private String date;
+    private String startTime;
+    private String endTime;
+    private Map<String,String> studentAttendance = new HashMap<>();
+    private static int N=1;
+
+
+
+    public Session(String date, String startTime, String endTime) {
+        this.sessionName = "Session " + N++;
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public String getSessionName() {
+        return sessionName;
+    }
+
+    public void setStudentAttendance(Map<String, String> studentAttendance) {
+        this.studentAttendance = studentAttendance;
+    }
+
+    public void markAttendance(List<String> attendedStudents) {
+        for (String id: attendedStudents){
+            studentAttendance.put(id,"Attended");
+        }
+
+    }
+
+
+
+    public Map<String, String> getStudentAttendance() {
+        return studentAttendance;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.date.hashCode() + 37 + this.startTime.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
+        }
+
+        return this.getClass().equals(obj.getClass());
+    }
+
+    @Override
+    public String toString() {
+        return  date + " :-" + startTime + " - " + endTime + "\n";
+    }
     
 
 }
