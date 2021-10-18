65036c886da6076124d635cac9f9c9ada83f26e7
Mohamed Nufail <eg173254@engug.ruh.ac.lk>
2021-10-18 17:32:39 +0530

    Added classDetails and classInMemoryStore

--- src/main/java/org/example/ClassDetails/ClassDetails.java
+++ src/main/java/org/example/ClassDetails/ClassDetails.java
@@ -12,13 +12,32 @@ public class ClassDetails {
     private String teacherName;
     private final List<Student> students;
 
+    private final Map<String, String> studentId =new HashMap<>();
+    private final Map<String, Session> sessionsAttendance = new HashMap<>();
+
     
 
 
 
-    public ClassDetails(String className) {
+    public ClassDetails(int year, String className, String teacherName,StudentStore studentStore) {
+        this.year = year;
         this.className = className;
-        this.students = new ArrayList<>();
+        this.teacherName = teacherName;
+        this.studentStore = studentStore;
+
+    }
+
+    public void addStudent(String id){
+        if (studentStore != null){
+           Student foundStudent = studentStore.getStudent(id);
+           if (foundStudent != null){
+               studentId.put(id,"");
+           }
+        }
+    }
+
+    public Map<String, Session> getSessionsAttendance() {
+        return sessionsAttendance;
     }
 
     public void setYear(int year) {
@@ -30,12 +49,15 @@ public class ClassDetails {
     }
 
 
-
     public int getYear() {
         return year;
     }
 
-     public String getClassName() {
+    public Map<String,Session> getSessions() {
+        return sessionsAttendance;
+    }
+
+    public String getClassName() {
         return className;
     }
 
@@ -43,10 +65,50 @@ public class ClassDetails {
         return teacherName;
     }
 
-    public List<Student> getStudents() {
-        return students;
+
+    public void reserveSession(Session session) {
+        sessionsAttendance.put(session.getSessionName(),session);
     }
 
+    public void markAttendance(String sessionName,List<String> attendedStudents) {
+        for (String id: attendedStudents){
+           studentId.put(id,"Attended");
+            sessionsAttendance.get(sessionName).markAttendance(attendedStudents);
+        }
+        sessionsAttendance.get(sessionName).setStudentAttendance(studentId);
+
+    }
+
+
+    @Override
+    public String toString() {
+        String s = "\nClass:\n";
+
+        s = s +  year + "-" + className + "\n" ;
+
+        s = s + "\nStudent Details\n";
+
+        if (studentStore != null){
+            for (Map.Entry<String,String> stu : studentId.entrySet()) {
+                Student foundStudent = studentStore.getStudent(stu.getKey());
+                s = s + foundStudent.getId() + " - " + foundStudent.getName() + " - " + stu.getValue()+ "\n";
+            }
+
+
+        for (Map.Entry<String, Session> ses : sessionsAttendance.entrySet()){
+            s = s + ses.getKey()  + " Number of attendance " + ses.getValue().getStudentAttendance().size() + "\n";
+
+//            s = s + "\nStudent Details\n";
+
+            for (Map.Entry<String,String> student : ses.getValue().getStudentAttendance().entrySet()){
+                s = s + studentStore.getStudent(student.getKey()) + " - " + student.getValue()  + "\n";
+            }
+        }
+
+        }
+
+        return s;
+    }
 
 
 
--- src/main/java/org/example/ClassDetails/ClassInMemoryStore.java
+++ src/main/java/org/example/ClassDetails/ClassInMemoryStore.java
@@ -6,39 +6,135 @@ import java.util.*;
 
 public class ClassInMemoryStore implements ClassDetailsStore{
 
+    Map<Integer, List<ClassDetails>> classDetails = new HashMap<>();
+    private final StudentStore studentStore;
+
+    public ClassInMemoryStore(StudentStore studentStore) {
+        this.studentStore = studentStore;
+    }
 
     @Override
     public ClassDetails getClassDetails(int grade, String className) {
 
+        if (classDetails.containsKey(grade)){
+            for (List<ClassDetails> s : classDetails.values()){
+                if (s.stream().noneMatch(c -> c.getClassName().equals(className))){
+                    return s.stream().filter(d -> d.getClassName().equals(className)).findFirst().get();
+                }
+            }
+
+        }
         return null;
     }
 
     @Override
     public void addClassDetails(int grade, int year, String className, String teacherName) {
-        
+        if (!classDetails.containsKey(grade)){
+            List<ClassDetails> list = new ArrayList<>();
+            list.add(new ClassDetails(year,className,teacherName,studentStore));
+            classDetails.put(grade,list);
+        }else {
+            List<ClassDetails> list = classDetails.get(grade);
+            list.add(new ClassDetails(year,className,teacherName,studentStore));
+            classDetails.put(grade, list);
+        }
     }
 
     @Override
     public void addStudents(int grade, String className,String id) {
 
+        if (!classDetails.containsKey(grade)){
+            throw new RuntimeException("Grade not exist");
+        }
+
+        if (classDetails.containsKey(grade)){
+            for (List<ClassDetails> s : classDetails.values()){
+                if (s.stream().noneMatch(c -> c.getClassName().equals(className))){
+                    throw new RuntimeException("Class name not exist");
+                }
+
+                ClassDetails foundClass = s.stream().filter(k -> k.getClassName().equals(className)).findFirst().get();
+                foundClass.addStudent(id);
+
+            }
+        }
 
     }
 
     @Override
     public void reserveSession(int grade, String className, Session session) {
+        if (!classDetails.containsKey(grade)){
+            throw new RuntimeException("Grade not exist");
+        }
+
+        if (classDetails.containsKey(grade)){
+            for (List<ClassDetails> s : classDetails.values()){
+                if (s.stream().noneMatch(c -> c.getClassName().equals(className))){
+                    throw new RuntimeException("Class name not exist");
+                }
+
+                ClassDetails foundClass = s.stream().filter(k -> k.getClassName().equals(className)).findFirst().get();
+                foundClass.reserveSession(session);
+
+            }
+        }
 
     }
 
     public Set<String> getSessionName(int grade, String className){
-       
+        if (!classDetails.containsKey(grade)){
+            throw new RuntimeException("Grade not exist");
+        }
+
+        if (classDetails.containsKey(grade)){
+            for (List<ClassDetails> s : classDetails.values()){
+                if (s.stream().noneMatch(c -> c.getClassName().equals(className))){
+                    throw new RuntimeException("Class name not exist");
+                }
+
+                ClassDetails foundClass = s.stream().filter(k -> k.getClassName().equals(className)).findFirst().get();
+               return foundClass.getSessions().keySet();
+
+            }
+        }
 
         return null;
     }
 
     @Override
     public void markAttendance(int grade, String className, String session,List<String> attendedStudents) {
-        
+        if (!classDetails.containsKey(grade)){
+            throw new RuntimeException("Grade not exist");
+        }
+
+        if (classDetails.containsKey(grade)){
+            for (List<ClassDetails> s : classDetails.values()){
+                if (s.stream().noneMatch(c -> c.getClassName().equals(className))){
+                    throw new RuntimeException("Class name not exist");
+                }
+
+                ClassDetails foundClass = s.stream().filter(k -> k.getClassName().equals(className)).findFirst().get();
+                foundClass.markAttendance(session,attendedStudents);
+
+            }
+        }
     }
 
-   
+    @Override
+    public String toString() {
+        String s=  "\n CLASS DETAILS \n" ;
+
+        for (Map.Entry<Integer,List<ClassDetails>> det : classDetails.entrySet()){
+            int grade = det.getKey();
+            List<ClassDetails> details = det.getValue();
+
+            s = s + "Grade: " + grade;
+            for (ClassDetails detail : details){
+                s = s + detail;
+            }
+        }
+
+        return s;
+
+    }
 }
